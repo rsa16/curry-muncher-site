6953fda555f7b264978f3fa9d1510c6dfd7c33b2
Rehan Ali <sipnet66@outlook.com>
2024-07-27 23:35:08 -0400

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import storyblok from '@storyblok/astro'
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                novel: "storyblok/Novel",
+                chapter: "storyblok/Chapter"
+            },
+            apiOptions: { region: "us" },
+        }),
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: { https: true }
+    },
+    image: {
+        domains: ["storyblok.com"]
+    }
+});
