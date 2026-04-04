f5ac95d11a114cf5187f968b66a7bcb7d57a81b9
Rehan Ali <sipnet66@outlook.com>
2026-04-04 00:14:22 -0400

    fix: add tailwind, astro icon, and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Instrument Serif",
+      cssVariable: "--font-instrument",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Poppins",
+      cssVariable: "--font-poppins"
+    }
+  ]
+});
